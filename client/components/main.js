104e537d94db47cd496fae8188a0b2a1489c54a0
BogdanShutko <67684562+BogdanShutko@users.noreply.github.com>
2020-09-25 12:23:54 +0200

    Update main.js

--- client/components/main.js
+++ client/components/main.js
@@ -7,8 +7,8 @@ const Main = () => {
     <div>
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
-        <div id="title" className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Main
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div id="title">Main</div>
           <div><Link to='/dashboard/profile/562dee83-02be-437d-b53a-afb5efee0695'>Go To Profile</Link></div>
           <div><Link to='/dashboard'>Go To Root</Link></div>
         </div>
